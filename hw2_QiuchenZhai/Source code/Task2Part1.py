97783e7e94820e50c51228950f3fc197c89a908b
Qiuchen Zhai <55102827+qiuchenzhai@users.noreply.github.com>
2020-10-28 20:16:51 -0400

    Add files via upload

---
+++ hw2_QiuchenZhai/Source code/Task2Part1.py
@@ -0,0 +1,158 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from skimage import io
+
+# read image
+img1 = io.imread('photoframe1.jpeg')
+img2 = io.imread('photoframe2.jpeg')
+img3 = io.imread('photoframe3.jpeg')
+img = io.imread('santaclaus.jpeg')
+
+# record the coordinates of points
+PQSR1 = np.array([[1816, 1165], [1836, 1875], [2586, 1232], [2622, 1839]])
+PQSR2 = np.array([[1450, 1060], [1380, 1864], [2702, 1042], [2776, 1866]])
+PQSR3 = np.array([[1297, 1173], [1255, 1959], [2385, 1114], [2373, 2020]])
+PQSR = np.array([[0, 0], [0, 1536], [2048, 0], [2048, 1536]])
+
+# plot images with ROI
+# # =====================================================================================================================
+# point = ["P", "Q", "S", "R"]
+#
+# plt.imshow(img1)
+# for i in range(4):
+#     plt.scatter(PQSR1[i][0], PQSR1[i][1])
+#     plt.annotate(point[i] + '({},{})'.format(PQSR1[i][0], PQSR1[i][1]), (PQSR1[i][0], PQSR1[i][1]), c='r')
+# plt.title('photoframe1.jpeg')
+# plt.axis('off')
+# plt.savefig('task2_labelled1.jpeg')
+# plt.clf()
+#
+# plt.imshow(img2)
+# for i in range(4):
+#     plt.scatter(PQSR2[i][0], PQSR2[i][1])
+#     plt.annotate(point[i] + '({},{})'.format(PQSR2[i][0], PQSR2[i][1]), (PQSR2[i][0], PQSR2[i][1]), c='r')
+# plt.title('photoframe2.jpeg')
+# plt.axis('off')
+# plt.savefig('task2_labelled2.jpeg')
+# plt.clf()
+# #
+# plt.imshow(img3)
+# for i in range(4):
+#     plt.scatter(PQSR3[i][0], PQSR3[i][1])
+#     plt.annotate(point[i] + '({},{})'.format(PQSR3[i][0], PQSR3[i][1]), (PQSR3[i][0], PQSR3[i][1]), c='r')
+# plt.title('photoframe3.jpeg')
+# plt.axis('off')
+# plt.savefig('task2_labelled3.jpeg')
+# plt.clf()
+#
+# plt.imshow(img)
+# for i in range(4):
+#     plt.scatter(PQSR[i][0], PQSR[i][1])
+#     plt.annotate(point[i] + '({},{})'.format(PQSR[i][0], PQSR[i][1]), (PQSR[i][0], PQSR[i][1]), c='r')
+# plt.title('santaclaus.jpeg')
+# plt.axis('off')
+# plt.savefig('task2_labelled4.jpeg')
+# plt.clf()
+
+
+def compute_hc(src, dst):
+    """
+    The module computes the homography matrix by AH = b.
+    :param src: source points.
+    :param dst: mapped points.
+    :return: homography matrix H.
+    """
+
+    A = np.zeros((8, 8))
+    b = np.zeros((8, 1))
+
+    for i in range(len(src)):
+        A[2 * i] = [src[i][0], src[i][1], 1, 0, 0, 0, -src[i][0]*dst[i][0], -src[i][1]*dst[i][0]]
+        A[2 * i + 1] = [0, 0, 0, src[i][0], src[i][1], 1, -src[i][0]*dst[i][1], -src[i][1]*dst[i][1]]
+        b[2 * i] = dst[i][0]
+        b[2 * i + 1] = dst[i][1]
+
+    h = np.dot(np.linalg.pinv(A), b)
+    homo_mat = np.append(h, 1)
+
+    return homo_mat.reshape((3, 3))
+
+
+def compute_pixel_val(img, loc):
+    """
+    This module returns the pixel value given by weighting average of neighbor pixels.
+    :param img: the mapping img.
+    :param loc: the coordinates of mapped points.
+    :return: the pixel value.
+    """
+
+    loc0_f = np.int(np.floor(loc[0]))
+    loc1_f = np.int(np.floor(loc[1]))
+    loc0_c = np.int(np.ceil(loc[0]))
+    loc1_c = np.int(np.ceil(loc[1]))
+    a = img[loc0_f][loc1_f]
+    b = img[loc0_f][loc1_c]
+    c = img[loc0_c][loc1_f]
+    d = img[loc0_c][loc1_c]
+    dx = float(loc[0] - loc0_f)
+    dy = float(loc[1] - loc1_f)
+    Wa = 1 / np.linalg.norm([dx, dy])
+    Wb = 1 / np.linalg.norm([1 - dx, dy])
+    Wc = 1 / np.linalg.norm([dx, 1 - dy])
+    Wd = 1 / np.linalg.norm([1 - dx, 1 - dy])
+    output = (a * Wa + b * Wb + c * Wc + d * Wd) / (Wa + Wb + Wc + Wd)
+
+    return output
+
+
+def mapping(src_img, dst_img, points_src, h_mat):
+    """
+    The mapping function maps the source image to the destination.
+    :param src_img: source image.
+    :param dst_img: the projection image.
+    :param points_src: the P', Q', S', R' points.
+    :param h_mat: the HC matrix H.
+    :return: the projected image.
+    """
+    temp = np.zeros(dst_img.shape[0:2])
+    pts = np.array([[points_src[0][0], points_src[0][1]], [points_src[1][0], points_src[1][1]], [points_src[3][0],
+                    points_src[3][1]], [points_src[2][0], points_src[2][1]]])
+    cv2.fillPoly(temp, [pts], 255)
+    # plt.imshow(temp)
+    # plt.show()
+    # plt.clf()
+    loc_hc = np.array([[np.dot(h_mat, np.array([j, i, 1])) for j in range(temp.shape[1])] for i in range(temp.shape[0])])
+    loc = np.array([loc_hc[:, :, 0] / loc_hc[:, :, 2], loc_hc[:, :, 1] / loc_hc[:, :, 2]])
+    for i in range(temp.shape[0]):
+        for j in range(temp.shape[1]):
+            loc0 = loc[1, i, j]
+            loc1 = loc[0, i, j]
+            if (loc0 > 0) and (loc1 > 0) and (loc0 < src_img.shape[0]-1) and (loc1 < src_img.shape[1]-1):
+                dst_img[i, j][temp[i, j] == 255] = compute_pixel_val(src_img, [loc0, loc1])
+
+    return dst_img
+
+
+# image projection
+h_ad = compute_hc(PQSR1, PQSR)
+dst_img = mapping(img, img1, PQSR1, h_ad)
+plt.imshow(dst_img)
+plt.axis('off')
+plt.savefig('task2_dtoa.jpeg')
+plt.clf()
+
+h_bd = compute_hc(PQSR2, PQSR)
+dst_img = mapping(img, img2, PQSR2, h_bd)
+plt.imshow(dst_img)
+plt.axis('off')
+plt.savefig('task2_dtob.jpeg')
+plt.clf()
+
+h_cd = compute_hc(PQSR3, PQSR)
+dst_img = mapping(img, img3, PQSR3, h_cd)
+plt.imshow(dst_img)
+plt.axis('off')
+plt.savefig('task2_dtoc.jpeg')
+plt.clf()
+
